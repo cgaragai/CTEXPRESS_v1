a2fbb6da0192aeb0a1d6b75f83a3612a073a86f3
Leonardo godoy campos <leo_thecure@hotmail.com>
2017-12-20 01:22:12 -0300

    camara

--- app/src/main/java/cl/android/trabajo/ctexpress/Vista/Tickets/GenerarTicket.java
+++ app/src/main/java/cl/android/trabajo/ctexpress/Vista/Tickets/GenerarTicket.java
@@ -1,8 +1,16 @@
 package cl.android.trabajo.ctexpress.Vista.Tickets;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -15,6 +23,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -41,6 +50,8 @@ public class GenerarTicket extends AppCompatActivity implements AdapterView.OnIt
     private Ticket ticketLocal;
     private boolean autoseleccionado, cargarDatos;
     private int conteoSpinner;
+    private final int MY_PERMISSIONS_REQUEST_CAMARA = 0;
+    private final int MY_PERMISSIONS_REQUEST_STORAGE = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -456,4 +467,58 @@ public class GenerarTicket extends AppCompatActivity implements AdapterView.OnIt
         }
         return ok;
     }
+
+    public void camara(View view) {
+        if (Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP_MR1) {// Marshmallow+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                } else {
+                    // No se necesita dar una explicación al usuario, sólo pedimos el permiso.
+                    ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMARA );
+                    // MY_PERMISSIONS_REQUEST_CAMARA es una constante definida en la app. El método callback obtiene el resultado de la petición.
+                }
+            }else{ //have permissions
+                abrirCamara();
+            }
+        }else{ // Pre-Marshmallow
+            abrirCamara();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMARA : {
+                // Si la petición es cancelada, el array resultante estará vacío.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // El permiso ha sido concedido.
+                    abrirCamara ();
+                } else {
+                    // Permiso denegado, deshabilita la funcionalidad que depende de este permiso.
+                }
+                return;
+            }
+            case MY_PERMISSIONS_REQUEST_STORAGE: {
+                // Si la petición es cancelada, el array resultante estará vacío.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // El permiso ha sido concedido.
+
+                } else {
+                    // Permiso denegado, deshabilita la funcionalidad que depende de este permiso.
+                }
+                return;
+            }
+            // otros bloques de 'case' para controlar otros permisos de la aplicación
+        }
+    }
+
+    private void abrirCamara() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
 }
